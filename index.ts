9b9bdfa7ac536ad97a40950737aaa546176e8edb
Son Tran <son.tran@team.enouvo.com>
2022-05-12 22:07:59 +0700

    Convert js to typescript

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+import * as path from 'path';
+const app = express();
+const port = 6001;
+
+app.use(express.static('public'));
+
+const VueApp = (res: Response) => res.sendFile(path.join(__dirname, '../public/vue-app/index.html'));
+const MusicApp = (res: Response) => res.sendFile(path.join(__dirname, '../public/music-app/index.html'));
+const ReactApp = (res: Response) => res.sendFile(path.join(__dirname, '../public/react-app/index.html'));
+const SvelteApp = (res: Response) => res.sendFile(path.join(__dirname, '../public/svelte-app/index.html'));
+
+app.get(['/vue-app', '/vue-app/*'], (_req: Request, res: Response) => {
+  return VueApp(res);
+});
+app.get(['/music-app', '/music-app/*'], (_req: Request, res: Response) => {
+  return MusicApp(res);
+});
+app.get(['/react-app', '/react-app/*'], (_req: Request, res: Response) => {
+  return ReactApp(res);
+});
+
+app.get(['/svelte-app', '/svelte-app/*'], (_req: Request, res: Response) => {
+  return SvelteApp(res);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
